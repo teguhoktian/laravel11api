a9e19ce19ee9f044d9ed4d641a8f628308977e5f
Hoki Teguh Oktian <oktian89@gmail.com>
2024-05-04 19:09:29 +0700

    Standarisasi Response API menggunakan Class APIResponseBuilder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,72 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\APIResponseBuilder;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
 
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
-        return User::with(['roles'])
+        $users = User::with(['roles'])
             ->search($request->search)
             ->orderBy('id', 'DESC')
             ->paginate($request->per_page ?: env('PER_PAGE'));
+
+        return APIResponseBuilder::success($users->setHidden(['email_verified_at', 'password', 'remember_token', 'updated_at']), __("Users list successfully loaded."));
     }
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        User::create([
+        if ($validator->fails()) return APIResponseBuilder::invalidData(__('Unprocessable Entity'), $validator->messages());
+
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        return response()->json([
-            'success' => true,
-            'message' => __('Data berhasil disimpan.')
-        ], 200);
+        return APIResponseBuilder::success($user, __("Data saved successfull."));
     }
 
     public function update(User $user, Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class . ",email," . $user->id]
         ]);
 
+        if ($validator->fails()) return APIResponseBuilder::invalidData(__('Unprocessable Entity'), $validator->messages());
+
         $request['password'] = $user->password;
         $user->update($request->only('name', 'email', 'password'));
 
-        return response()->json([
-            'success' => true,
-            'message' => __('Data berhasil diubah.')
-        ], 200);
+        return APIResponseBuilder::success($user, __("Data updated successfull."));
     }
 
     public function show(User $user)
     {
         $user->role = $user->roles->pluck(['name']);
-        return $user->setHidden(['email_verified_at', 'password', 'remember_token', 'updated_at', 'roles']);
+        return APIResponseBuilder::success($user->setHidden(['email_verified_at', 'password', 'remember_token', 'updated_at', 'roles']));
     }
 
     public function destroy(User $user)
     {
+        $u = $user;
         $user->delete();
-
-        return response()->json([
-            'success' => true,
-            'message' => __('Data berhasil dihapus.')
-        ], 200);
+        return APIResponseBuilder::success($u->setHidden(['email_verified_at', 'password', 'remember_token', 'updated_at', 'roles']), __('Data deleted successfully'));
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,17 @@
 <?php
 
+use App\APIResponseBuilder;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -23,5 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Error Handling for Model Binding Route Exceptions
+        $exceptions->render(function (NotFoundHttpException $e): JsonResponse {
+            return APIResponseBuilder::notFound();
+        });
     })->create();
