6de3b5466e92cad88f65c3607b4251428841535f
Hoki Teguh Oktian <oktian89@gmail.com>
2024-05-04 15:48:07 +0700

    Seeder Command untuk User awal beserta Permissionnya

---
+++ app/Models/Permission.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Permission as ModelsPermission;
+
+class Permission extends ModelsPermission
+{
+    use HasFactory;
+
+    public static function defaultPermissions(): array
+    {
+        return [
+            'view_users',
+            'add_users',
+            'edit_users',
+            'delete_users',
+
+            'view_roles',
+            'add_roles',
+            'edit_roles',
+            'delete_roles',
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,63 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        if ($this->command->confirm('Jalankan migrate:refresh sebelum seeding?')) {
+            $this->command->call('migrate:refresh');
+            $this->command->warn("Database telah dikosongkan!");
+        }
+
+        // Masukkan Permissions Awal
+        $permissions = Permission::defaultPermissions();
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        $this->command->info('Default Permission added.');
+
+        if ($this->command->confirm('Buat Role untuk Pengguna, admin dan user sebagai bawaan? [y|N]', true)) {
+
+            $input_roles = $this->command->ask('Masukkan role dalam format terpisah koma.', 'Admin,User');
+
+            $roles_array = explode(',', $input_roles);
+
+            foreach ($roles_array as $role) {
+                $role = Role::firstOrCreate(['name' => trim($role)]);
+
+                if ($role->name == 'Admin') {
+
+                    $role->syncPermissions(Permission::all());
+                    $this->command->info('Admin diberikan semua permission');
+                } else {
+
+                    $role->syncPermissions(Permission::where('name', 'LIKE', 'view_%')->get());
+                }
+
+                // create one user for each role
+                $this->createUser($role);
+            }
+
+            $this->command->info('Roles ' . $input_roles . ' berhasil ditambahkan');
+        } else {
+            Role::firstOrCreate(['name' => 'User']);
+            $this->command->info('Added only default user role.');
+        }
+    }
+
+    function createUser($role): void
+    {
+        $user = \App\Models\User::factory()->create();
+        $user->assignRole($role->name);
+
+        if ($role->name == 'Admin') {
+            $this->command->info('Here is your admin details to login:');
+            $this->command->warn($user->email);
+            $this->command->warn('Password is "password"');
+        }
     }
 }
