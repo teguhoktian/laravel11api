4d57d7234998db6b2ec86d9ff073c08ceb09987e
Hoki Teguh Oktian <oktian89@gmail.com>
2024-05-09 04:37:32 +0700

    Perbaikan masalah database command db:seed pada Settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,25 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        try {
+            $settings = app(GeneralSetting::class);
+            config(['app.timezone' => $settings->timezone]);
+            config(['app.locale' => $settings->locale]);
+            config(['app.url' => $settings->site_url]);
+            config(['app.asset_url' => $settings->site_url]);
+            config(['app.name' => $settings->site_name]);
+        } catch (\Throwable $th) {
+            // No Action
+        }
+
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
-
-        $this->siteSettings();
-    }
-
-    /**
-     * Site Setting Method
-     * 
-     * @return void
-     */
-    public function siteSettings(): void
-    {
-        $settings = app(GeneralSetting::class);
-        config(['app.timezone' => $settings->timezone]);
-        config(['app.locale' => $settings->locale]);
-        config(['app.url' => $settings->site_url]);
-        config(['app.asset_url' => $settings->site_url]);
-        config(['app.name' => $settings->site_name]);
     }
 }
